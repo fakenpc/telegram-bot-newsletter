a8ad59ac2b6f6560613b5d76d7bcda5ddd5abe60
android <fakenpc>
2018-04-23 22:29:06 +0300

    tables structure

--- db.sql
+++ db.sql
@@ -1,131 +1,113 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Хост: localhost
--- Время создания: Апр 06 2018 г., 04:29
--- Версия сервера: 5.7.21-0ubuntu0.16.04.1
--- Версия PHP: 5.6.35-1+ubuntu16.04.1+deb.sury.org+1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- База данных: `telegram-bot-newsletter`
---
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `field`
---
-
-CREATE TABLE IF NOT EXISTS `field` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `field`;
+CREATE TABLE `field` (
+  `id` int(11) NOT NULL,
   `name` text COLLATE utf8mb4_unicode_520_ci,
   `value` text COLLATE utf8mb4_unicode_520_ci,
   `type` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `order` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `type` (`type`)
+  `order` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `newsletter`
---
-
-CREATE TABLE IF NOT EXISTS `newsletter` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `newsletter`;
+CREATE TABLE `newsletter` (
+  `id` int(11) NOT NULL,
   `newsletter_category_id` int(11) DEFAULT NULL,
   `name` text COLLATE utf8mb4_unicode_520_ci,
   `description` text COLLATE utf8mb4_unicode_520_ci,
   `sending_timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `disabling_timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `sended` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `sended` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `newsletter_category`
---
-
-CREATE TABLE IF NOT EXISTS `newsletter_category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `newsletter_category`;
+CREATE TABLE `newsletter_category` (
+  `id` int(11) NOT NULL,
   `name` text COLLATE utf8mb4_unicode_520_ci,
-  `description` text COLLATE utf8mb4_unicode_520_ci,
-  PRIMARY KEY (`id`)
+  `description` text COLLATE utf8mb4_unicode_520_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `newsletter_sended`
---
-
-CREATE TABLE IF NOT EXISTS `newsletter_sended` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `newsletter_sended`;
+CREATE TABLE `newsletter_sended` (
+  `id` int(11) NOT NULL,
   `subscriber_id` int(11) DEFAULT NULL,
-  `newsletter_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `newsletter_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `subscriber`
---
-
-CREATE TABLE IF NOT EXISTS `subscriber` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `subscriber`;
+CREATE TABLE `subscriber` (
+  `id` int(11) NOT NULL,
   `newsletter_category_id` int(11) NOT NULL,
   `subscription_id` int(11) NOT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   `chat_id` bigint(20) NOT NULL,
   `start_timestamp` int(11) DEFAULT NULL,
   `end_timestamp` int(11) DEFAULT NULL,
-  `paid` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `paid` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `subscription`
---
-
-CREATE TABLE IF NOT EXISTS `subscription` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `subscription`;
+CREATE TABLE `subscription` (
+  `id` int(11) NOT NULL,
+  `newsletter_category_id` int(11) NOT NULL DEFAULT '0',
   `name` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
   `duration` int(11) NOT NULL DEFAULT '1',
-  `price` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `price` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `trial`
---
-
-CREATE TABLE IF NOT EXISTS `trial` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `trial`;
+CREATE TABLE `trial` (
+  `id` int(11) NOT NULL,
   `user_id` bigint(20) NOT NULL,
-  `used` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `newsletter_category_id` int(11) NOT NULL DEFAULT '0',
+  `used` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 
+ALTER TABLE `field`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type` (`type`);
+
+ALTER TABLE `newsletter`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `newsletter_category`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `newsletter_sended`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `subscriber`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `subscription`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `trial`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `field`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `newsletter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `newsletter_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `newsletter_sended`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `subscriber`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `subscription`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `trial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
